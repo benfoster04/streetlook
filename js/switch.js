bfd6a2bb1914d419be792903e8aa13463657962e
Ben Foster <fosterben150@gmail.com>
2020-03-04 09:38:14 +0000

    Update switch.js

--- js/switch.js
+++ js/switch.js
@@ -2,8 +2,7 @@ window.addEventListener("DOMContentLoaded",function() {
     document.getElementById("switch").addEventListener("click", function() {
         var x = document.body.classList.toggle("dark");
         document.body.classList.toggle("light");
-        if ( x ) {
-            document.cookie = "theme=dark;SameSite=Strict;expires=";
-        }
+        if ( x ) {document.cookie = "theme=dark;SameSite=Strict;expires=";}
+        else {document.cookie = "theme=light;SameSite=Strict;expires=";}
     })
 });
